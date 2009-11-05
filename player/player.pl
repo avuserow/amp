e353242c9ef9068b8fe451e9bf807a14c000fc49
Alex Van Damme <atv@ubuntu.(none)>
2009-11-05 06:43:02 -0600

    delete from votes table on play

--- player/player.pl
+++ player/player.pl
@@ -8,6 +8,7 @@ my $db = DBI->connect("DBI:SQLite:../acoustics.db","","",{RaiseError=>1, AutoCom
 my $select = $db->prepare("SELECT song_id FROM votes GROUP BY song_id ORDER BY count(song_id) DESC");
 my $song = $db->prepare("SELECT * FROM songs WHERE song_id=?");
 my $random = $db->prepare("SELECT * FROM songs ORDER BY RANDOM() LIMIT 1");
+my $delete = $db->prepare("DELETE FROM votes WHERE song_id=?");
 
 #while(1)
 {
@@ -28,5 +29,7 @@ my $random = $db->prepare("SELECT * FROM songs ORDER BY RANDOM() LIMIT 1");
 		%data = %{$random->fetchrow_hashref()};
 	}
 
+	$delete->execute($data{song_id});
+	#TODO: update player table
 	system("vlc", "-Irc", $data{path}, "vlc://quit");
 }
