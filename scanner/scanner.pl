e3dfe5ecc300a25a7aaba192762276425170468d
Alex Van Damme <atv@ubuntu.(none)>
2009-11-05 05:32:03 -0600

    unique files

--- scanner/scanner.pl
+++ scanner/scanner.pl
@@ -5,21 +5,24 @@ use warnings;
 use DBI;
 use File::Find::Rule ();
 use List::MoreUtils qw(uniq);
+use Cwd qw(abs_path);
 
+#database creation, move later
 my $db = DBI->connect("DBI:SQLite:../acoustics.db","","",{RaiseError=>1, AutoCommit=>1});
 
 $db->do("CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR, path VARCHAR, artist VARCHAR, album VARCHAR, title VARCHAR, length INTEGER)");
 $db->do("CREATE TABLE IF NOT EXISTS votes (song_id VARCHAR, who VARCHAR, player_id VARCHAR, time DATETIME)");
 $db->do("CREATE TABLE IF NOT EXISTS players (player VARCHAR, volume INTEGER, song_id VARCHAR)");
 
-my @files = uniq(File::Find::Rule->file()->in(@ARGV));
+#get list of unique filenames from paths passed on command line
+my @files = uniq(map {abs_path($_)} File::Find::Rule->file()->in(@ARGV));
 
-
-
+#pass filenames through tagreader
 open my $pipe, '-|', './tagreader', @files or die "couldn't open tagreader: $!";
 my $data = join '', <$pipe>;
 close $pipe;
 
+#split apart data, insert to database
 my @datas = split /---/, $data;
 my $sth = $db->prepare("INSERT INTO songs (path, artist, album, title, length) VALUES (?, ?, ?, ?, ?)");
 $db->begin_work();
