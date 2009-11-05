bcba12b644cf7dfb2f1312d27838a2642bf66e05
Alex Van Damme <atv@ubuntu.(none)>
2009-11-05 05:28:09 -0600

    process directories

--- scanner/scanner.pl
+++ scanner/scanner.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 use DBI;
+use File::Find::Rule ();
+use List::MoreUtils qw(uniq);
 
 my $db = DBI->connect("DBI:SQLite:../acoustics.db","","",{RaiseError=>1, AutoCommit=>1});
 
@@ -10,7 +12,11 @@ $db->do("CREATE TABLE IF NOT EXISTS songs (song_id VARCHAR, path VARCHAR, artist
 $db->do("CREATE TABLE IF NOT EXISTS votes (song_id VARCHAR, who VARCHAR, player_id VARCHAR, time DATETIME)");
 $db->do("CREATE TABLE IF NOT EXISTS players (player VARCHAR, volume INTEGER, song_id VARCHAR)");
 
-open my $pipe, '-|', './tagreader', @ARGV or die "couldn't open tagreader: $!";
+my @files = uniq(File::Find::Rule->file()->in(@ARGV));
+
+
+
+open my $pipe, '-|', './tagreader', @files or die "couldn't open tagreader: $!";
 my $data = join '', <$pipe>;
 close $pipe;
 
@@ -19,6 +25,12 @@ my $sth = $db->prepare("INSERT INTO songs (path, artist, album, title, length) V
 $db->begin_work();
 for my $item (@datas) {
 	my %hash = map {(split /:/, $_, 2)} split /\n/, $item;
+	unless($hash{length})
+	{
+		print "file $hash{path} not music\n";
+		next;
+	}
+	print "file $hash{path} processed\n";
 	$sth->execute((map {$hash{$_}} qw(path artist album title length)));
 }
 $db->commit();
