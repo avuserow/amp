5c8dacf5086f312b4bf5533b6ac39b8d90511da5
Alex Van Damme <atv@ubuntu.(none)>
2009-11-05 06:57:22 -0600

    history, delete songs from db if no longer exist on disk

--- initdb.pl
+++ initdb.pl
@@ -8,7 +8,8 @@ my $db = DBI->connect("DBI:SQLite:./acoustics.db","","",{RaiseError=>1, AutoComm
 
 $db->do("DROP TABLE IF EXISTS songs");
 $db->do("DROP TABLE IF EXISTS votes");
+$db->do("DROP TABLE IF EXISTS history");
 
 $db->do("CREATE TABLE songs (song_id INTEGER PRIMARY KEY AUTOINCREMENT, path VARCHAR, artist VARCHAR, album VARCHAR, title VARCHAR, length INTEGER)");
 $db->do("CREATE TABLE votes (song_id INTEGER, who VARCHAR, player_id VARCHAR, time DATETIME)");
-
+$db->do("CREATE TABLE history (song_id INTEGER, pretty_name VARCHAR, time DATETIME, who VARCHAR)");
--- player/player.pl
+++ player/player.pl
@@ -9,6 +9,8 @@ my $select = $db->prepare("SELECT song_id FROM votes GROUP BY song_id ORDER BY c
 my $song = $db->prepare("SELECT * FROM songs WHERE song_id=?");
 my $random = $db->prepare("SELECT * FROM songs ORDER BY RANDOM() LIMIT 1");
 my $delete = $db->prepare("DELETE FROM votes WHERE song_id=?");
+my $history_insert = $db->prepare("INSERT INTO history (song_id, who, time, pretty_name) VALUES(?,?,?,?)");
+my $garbage = $db->prepare("DELETE FROM songs WHERE path=?");
 
 #while(1)
 {
@@ -30,6 +32,15 @@ my $delete = $db->prepare("DELETE FROM votes WHERE song_id=?");
 	}
 
 	$delete->execute($data{song_id});
-	#TODO: update player table
-	system("vlc", "-Irc", $data{path}, "vlc://quit");
+
+	if(-e $data{path})
+	{
+		$history_insert->execute($data{song_id}, '', time(), "$data{artist} - $data{title}");
+		#TODO: update player table
+		system("vlc", "-Irc", $data{path}, "vlc://quit");
+	}
+	else
+	{
+		$garbage->execute($data{path});
+	}
 }
