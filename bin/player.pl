dd7039c18ee553e6660e89fc04b057f1c504f0a0
Adrian Kreher <ak@urbania.localdomain>
2009-11-09 22:32:31 -0600

    use mplayer instead of vlc. sighup skips songs.

--- bin/player.pl
+++ bin/player.pl
@@ -5,6 +5,7 @@ use warnings;
 use lib ($0 =~ m{(.+)/})[0] . '/../lib';
 use Acoustics;
 use Log::Log4perl ':easy';
+use IPC::Open2 'open2';
 
 my $acoustics = Acoustics->new({
 	data_source => ($0 =~ m{(.+)/})[0] . '/../acoustics.db',
@@ -23,12 +24,31 @@ while(1)
 		$acoustics->add_playhistory(\%data);
 		$acoustics->update_player({song_id => $data{song_id}});
 		INFO "Playing '$data{path}'";
-		system("vlc", "-Irc", $data{path}, "vlc://quit");
+
+		# General plan: open both input and output of the mplayer process
+		# then continually read from input so we know it's still running
+		# and handle SIGHUP. don't use waitpid because it blocks SIGHUP.
+		my $pid = open2(my $child_out, my $child_in,
+			'mplayer', '-slave', '-quiet', $data{path})
+			or LOGDIE "couldn't open mplayer: $!";
+
+		# when we get SIGHUP, ask mplayer to quit
+		$SIG{HUP} = sub {
+			WARN "skipping song: $data{path}!\n";
+			print $child_in "quit\n";
+		};
+
+		# loop until mplayer ends
+		while (<$child_out>) {}
+
+		close $child_out; close $child_in;
+
+		# restore SIGHUP to reasonable stuff
+		$SIG{HUP} = 'IGNORE';
 	}
 	else
 	{
 		ERROR "Song '$data{path}' is invalid, deleting";
 		$acoustics->delete_song({song_id => $data{song_id}});
 	}
-	sleep(1);
 }
