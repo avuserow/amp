54bb0877abc2b508e615636e73e3b8a883597c12
Daniel Freedman <dfreedm2@uiuc.edu>
2010-01-25 01:41:33 -0600

    Compiled acoustics.js with Closure Compiler, moved source to source.js. Use compile_javascript.sh to compile it

---
+++ compile_javascript.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -d closure-library-read-only ]; then
+    echo "You need the Google Closure library!";
+    exit 1;
+fi
+if [ ! -d closure-compiler ]; then
+    echo "You need the Google Closure Compiler!";
+fi
+python closure-library-read-only/closure/bin/calcdeps.py -i www-data/source.js -p closure-library-read-only -o compiled -c closure-compiler/compiler.jar > www-data/acoustics.js
