6841edc3722de3d5adcb8f1e377aaa074e05239e
Daniel Freedman <dfreedm2@uiuc.edu>
2010-01-25 02:43:26 -0600

    should exit if no compiler

--- compile_javascript.sh
+++ compile_javascript.sh
@@ -6,5 +6,6 @@ if [ ! -d closure-library-read-only ]; then
 fi
 if [ ! -d closure-compiler ]; then
     echo "You need the Google Closure Compiler!";
+    exit 1;
 fi
 python closure-library-read-only/closure/bin/calcdeps.py -i www-data/source.js -p closure-library-read-only -o compiled -c closure-compiler/compiler.jar > www-data/acoustics.js
