7acce54faf36af04760611997c44f3109e51b91a
Daniel Freedman <dfreedm2@uiuc.edu>
2010-01-27 00:01:38 -0600

    Moved tagreader bits to module, more to move here

---
+++ lib/Acoustics/Scanner.pm
@@ -0,0 +1,18 @@
+package Acoustics::Scanner;
+require Exporter;
+@ISA=qw(Exporter);
+@EXPORT_OK = qw(file_to_info);
+sub file_to_info {
+	my $file = shift;
+	#pass filenames through tagreader
+	open my $pipe, '-|', ($0 =~ m{(.+/)?})[0] . 'tagreader', $file or die "couldn't open tagreader: $!";
+	chomp(my @data = <$pipe>);
+	close $pipe;
+
+#split apart data, insert to database
+	my %hash = map {(split /:/, $_, 2)} @data;
+	delete $hash{bitrate}; # no bitrate field in the database yet
+	$hash{online} = 1; # set the online bit
+	return %hash;
+}
+1;
