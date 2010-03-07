49260bd8fd00e4975c86939b60a504879e65d013
Daniel Freedman <dfreedm2@uiuc.edu>
2010-03-07 14:22:08 -0600

    volume switch is unhappiness

--- lib/Acoustics/Player/MPlayer.pm
+++ lib/Acoustics/Player/MPlayer.pm
@@ -147,7 +147,7 @@ sub player_loop {
 		my $pid = open2(my $child_out, my $child_in,
 			'mplayer', '-slave', '-quiet',
 			'-af' => 'volnorm=2:0.10',
-			'-volume' => $player->{volume},
+			#'-volume' => $player->{volume},
 			$song->{path})
 			or LOGDIE "couldn't open mplayer: $!";
 
