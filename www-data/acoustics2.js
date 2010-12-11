72e25027cb52231b4a4c3e99de42eac83fc18abd
Adrian Kreher <avuserow@gmail.com>
2010-12-10 21:12:23 -0600

    [wip] [new-ui] player control buttons

--- www-data/acoustics2.js
+++ www-data/acoustics2.js
@@ -1,3 +1,4 @@
+var volume;
 var stateTimer;
 var templates = {};
 var jsonSource = 'json.pl';
@@ -37,6 +38,14 @@ function playerStateRequest() {
 }
 
 function handlePlayerStateRequest(json) {
+	// volume
+	if (json.player && json.player.volume) {
+		volume = parseInt(json.player.volume);
+		$("#controls-volume").html(volume);
+	} else {
+		$("#controls-volume").html("-");
+	}
+
 	// now playing
 	var nowPlaying = json.now_playing;
 	var nowPlayingPanel = templates.nowPlayingPanel.clone();
@@ -64,5 +73,46 @@ function handlePlayerStateRequest(json) {
 		$(".queue-song-title", entry).html(song.title);
 		$(".queue-song-artist", entry).html(song.artist);
 		entry.appendTo("#queue-list");
+	}
+}
+
+function controlPlayPause() {
+	$.getJSON(
+		jsonSource + '?mode=start',
+		function (data) {handlePlayerStateRequest(data);}
+	);
+}
+
+function controlStop() {
+	$.getJSON(
+		jsonSource + '?mode=stop',
+		function (data) {handlePlayerStateRequest(data);}
+	);
+}
+
+function controlNext() {
+	$.getJSON(
+		jsonSource + '?mode=skip',
+		function (data) {handlePlayerStateRequest(data);}
+	);
+}
+
+function controlVolumeDown() {
+	if (volume) {
+		volume -= 10;
+		$.getJSON(
+			jsonSource + '?mode=volume;value=' + volume,
+			function (data) {handlePlayerStateRequest(data);}
+		);
+	}
+}
+
+function controlVolumeUp() {
+	if (volume) {
+		volume += 10;
+		$.getJSON(
+			jsonSource + '?mode=volume;value=' + volume,
+			function (data) {handlePlayerStateRequest(data);}
+		);
 	}
 }
